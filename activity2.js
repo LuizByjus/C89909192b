3b47ca5d43dc53f78a1400a2c78685e798277053
LuizByjus <93152514+LuizByjus@users.noreply.github.com>
2021-12-07 18:08:42 -0300

    Update activity2.js

--- activity2.js
+++ activity2.js
@@ -5,6 +5,6 @@ window.location = "activity_1.html";
 
 function getScore()
 {
-	score = localStorage.getItem("score");	
-	document.getElementById("update").innerHTML = "<h1>Pontuação: " + score +"</h1>";
+	score = localStorage.getItem("Score");	
+	document.getElementById("update").innerHTML = "<h1>Pontuação:" + score +"</h1>";
 }
